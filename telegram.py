a0ab8536c1d4109ef37791e9e5c3b35a77448658
bryanngzh <bryanngzihao@gmail.com>
2023-01-15 09:54:41 +0800

    Fix overview

--- telegram.py
+++ telegram.py
@@ -11,6 +11,7 @@ import operator
 import pymongo
 from pymongo import MongoClient
 import certifi
+import copy
 
 # Database setup
 cluster = MongoClient("mongodb+srv://bryan:bryan@cluster0.fmdfb.mongodb.net/splitease?retryWrites=true&w=majority",
@@ -26,6 +27,7 @@ bot = telebot.TeleBot(API_KEY)
 
 bot.set_my_commands([
     BotCommand('start', 'Starts the bot'),
+    BotCommand('newtrip', 'Creates a new trip'),
     BotCommand('delete', 'Deletes a trip'),
     BotCommand('edit', 'Edits a trip'),
     BotCommand('trips', 'View all trips'),
@@ -68,7 +70,7 @@ def newtrip(message):
         return
     
     #Add to Database
-    if collection_trips.find_one({"tripname" : country, "chatid": chat_id} == None):
+    if collection_trips.find_one({"tripname" : country, "chatid": chat_id}) == None:
         result = {"tripname" : country, "chatid": chat_id}
         collection_trips.insert_one(result)
         print(collection_trips.find_one())
@@ -78,17 +80,18 @@ def newtrip(message):
 
     # Create the inline keyboard with the buttons
     keyboard = InlineKeyboardMarkup()
-    button1 = InlineKeyboardButton("Add me to the trip", callback_data="add_to_trip")
+    button1 = InlineKeyboardButton("Add me to the trip", callback_data="add_to_trip" + country)
     button2 = InlineKeyboardButton("Show the list", callback_data="show_list")
     keyboard.add(button1, button2)
     # Send the message with the inline keyboard
     bot.send_message(chat_id, f"Click the button to add your name for your {country} trip", reply_markup=keyboard)
 
 # Handle callback queries
-@bot.callback_query_handler(func=lambda call: call.data == "add_to_trip")
+@bot.callback_query_handler(func=lambda call: call.data.startswith("add_to_trip"))
 def add_to_trip(call):
     # Get the user's name
     username = call.from_user.username
+    country = call.data.replace("add_to_trip", "")
     # Add the user's username to the list
     if list_of_names.__contains__(username): 
         bot.answer_callback_query(call.id, f"Your username {username} has already been added to the trip!")  
@@ -97,8 +100,8 @@ def add_to_trip(call):
     chat_id = call.message.chat.id
 
     #Add user to database if user is not already present in database
-    if users.find_one({"username": username, "chatid": chat_id}) == None:
-        users.insert_one({"chatid": chat_id, "amount_spent": 0, "amount_paid": 0 ,"username": username})
+    if users.find_one({"username": username, "chatid": chat_id, "tripname": country}) == None:
+        users.insert_one({"chatid": chat_id, "amount_spent": 0, "amount_paid": 0 ,"username": username, "tripname": country})
         
     bot.answer_callback_query(call.id, f"Your username {username} has been added to the trip!")
     
@@ -169,47 +172,59 @@ def overview(message):
     """
 
     chat_id = message.chat.id
-    results = users.find({"chatid": chat_id})
+    country = message.text.split()[1]
+    results = list(users.find({"chatid": chat_id, "tripname": country}))        
     response = ""
     counter = 1
-
-    # # Initialize an empty dictionary to store the results
-    # owes = {}
-
-    # # Iterate over the people dictionary
-    # for result in results:
-    #     paid = result["amount_paid"]
-    #     spent = result["amount_spent"]
-    #     balance = paid - spent
-        
-    #     # If the person has a positive balance, they owe money
-    #     if balance > 0:
-    #         # Iterate over the people dictionary again to find the person to whom they owe money
-    #         for x in results:
-    #             if x["username"] != result["username"]:
-    #                 balance2 = x["amount_paid"] - x["amount_spent"]
-    #                 if balance2 < 0:
-    #                     # If the other person has a negative balance, they need to be paid
-    #                     amount = min(abs(balance), abs(balance2))
-    #                     # Add the transaction to the owes dictionary
-    #                     if result["username"] not in owes:
-    #                         owes[result["username"]] = {}
-    #                     if x["username"] not in owes[result["username"]]:
-    #                         owes[result["username"]][x["username"]] = amount
-    #                     else:
-    #                         owes[result["username"]][x["username"]] += amount
-    #                     balance += amount
-    #                     balance2 += amount
-
-    # print(owes)
     
-    for result in results :
-        response += str(counter) + ". " + result["username"] + "\n"
-        response += "Amount Spent: <b>" + str(result["amount_spent"]) + "</b>\n"
-        response += "Amount Paid: <b>" + str(result["amount_paid"]) + "</b>\n\n"
+    
+    owes = {}
+    result_count = len(results)
+    print("Number of results:", result_count)
+
+    results_copy = copy.deepcopy(results)
+
+    for result in results_copy:
+        paid = result["amount_paid"]
+        spent = result["amount_spent"]
+        balance = paid - spent
+        # If the person has a positive balance, they need to get paid
+        if balance > 0:
+            # Iterate over the people dictionary again to find the person to whom they owe money
+            for x in results_copy:
+                if x["username"] != result["username"]:
+                    balance2 = x["amount_paid"] - x["amount_spent"]
+                    if balance2 < 0:
+                        # If the other person has a negative balance, they need to pay others
+                        amount = min(abs(balance), abs(balance2))
+                        # Add the transaction to the owes dictionary
+                        if result["username"] not in owes:
+                            owes[result["username"]] = {}
+                        if x["username"] not in owes[result["username"]]:
+                            owes[result["username"]][x["username"]] = amount
+                        else:
+                            owes[result["username"]][x["username"]] += amount
+                        balance -= amount
+                        x["amount_paid"] += amount
+                        if (balance == 0):
+                            break
+
+    for result in results:
+        response = response + str(counter) + ". " + result["username"] + "\n"
+        response = response + "Amount Spent: <b>" + str(result["amount_spent"]) + "</b>\n"
+        response = response + "Amount Paid: <b>" + str(result["amount_paid"]) + "</b>\n"
+        response = response + "Amount Owed: <b>" + str(result["amount_paid"] - result["amount_spent"]) + "</b>\n\n"
+        if result["username"] in owes:
+            for key, value in owes[result["username"]].items():
+                response = response + "Owes: " + str(key) + ": <b>" + str(value) + "</b>\n"
+        response += "\n"
         counter = counter + 1
 
-    bot.send_message(chat_id, response, parse_mode= 'HTML')
+    if response:
+        bot.send_message(chat_id, response, parse_mode='HTML')
+    else:
+        bot.send_message(chat_id, "Error: response text is empty")
+
 
 # Running the bot
 while True:
